2dd0b2c2351f4418ee6aef234fc7256ffdc7def8
yacine <ramdaneyacine31@gmail.com>
2022-04-06 13:43:42 +0100

    v5

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  env: {},
+  env: {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
+  },
 };
 
 module.exports = nextConfig;
